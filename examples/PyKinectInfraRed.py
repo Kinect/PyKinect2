69b7fe51851b8712ef38be102530a5fadf7827c1
Philippe Grassia <pgrassia@bsd.uchicago.edu>
2015-05-27 15:52:16 -0500

    infrared example

--- examples/PyKinectInfraRed.py
+++ examples/PyKinectInfraRed.py
@@ -31,13 +31,6 @@ class InfraRedRuntime(object):
 		# Used to manage how fast the screen updates
 		self._clock = pygame.time.Clock()
 
-		# Set the width and height of the screen [width, height]
-		self._infoObject = pygame.display.Info()
-		self._screen = pygame.display.set_mode((self._infoObject.current_w >> 1, self._infoObject.current_h >> 1), 
-											   pygame.HWSURFACE|pygame.DOUBLEBUF|pygame.RESIZABLE, 32)
-
-		pygame.display.set_caption("Kinect for Windows v2 InfraRed")
-
 		# Loop until the user clicks the close button.
 		self._done = False
 
@@ -45,13 +38,19 @@ class InfraRedRuntime(object):
 		self._clock = pygame.time.Clock()
 
 		# Kinect runtime object, we want only color and body frames 
-		self._kinect = PyKinectRuntime.PyKinectRuntime(PyKinectV2.FrameSourceTypes_Infrared | PyKinectV2.FrameSourceTypes_Body)
+		self._kinect = PyKinectRuntime.PyKinectRuntime(PyKinectV2.FrameSourceTypes_Infrared)
 
 		# back buffer surface for getting Kinect infrared frames, 8bit grey, width and height equal to the Kinect color frame size
-		self._frame_surface = pygame.Surface((self._kinect.infrared_frame_desc.Width, self._kinect.infrared_frame_desc.Height), 0, 8)
-		print (self._kinect.infrared_frame_desc.Width, self._kinect.infrared_frame_desc.Height)
+		self._frame_surface = pygame.Surface((self._kinect.infrared_frame_desc.Width, self._kinect.infrared_frame_desc.Height), 0, 24)
 		# here we will store skeleton data 
 		self._bodies = None
+		
+		# Set the width and height of the screen [width, height]
+		self._infoObject = pygame.display.Info()
+		self._screen = pygame.display.set_mode((self._kinect.infrared_frame_desc.Width, self._kinect.infrared_frame_desc.Height), 
+											   pygame.HWSURFACE|pygame.DOUBLEBUF|pygame.RESIZABLE, 32)
+
+		pygame.display.set_caption("Kinect for Windows v2 Infrared")
 
 
     def draw_body_bone(self, joints, jointPoints, color, joint0, joint1):
@@ -116,67 +115,64 @@ class InfraRedRuntime(object):
 						   # it works with Kinect studio though
 			return
 		target_surface.lock()
-		frame8bit=np.uint8(192*frame/frame.max())
+		f8=np.uint8(frame.clip(1,4000)/16.)
+		frame8bit=np.dstack((f8,f8,f8))
 		address = self._kinect.surface_as_array(target_surface.get_buffer())
 		ctypes.memmove(address, frame8bit.ctypes.data, frame8bit.size)
 		del address
 		target_surface.unlock()
 
     def run(self):
-        # -------- Main Program Loop -----------
-        while not self._done:
-            # --- Main event loop
-            for event in pygame.event.get(): # User did something
-                if event.type == pygame.QUIT: # If user clicked close
-                    self._done = True # Flag that we are done so we exit this loop
+		# -------- Main Program Loop -----------
+		while not self._done:
+			# --- Main event loop
+			for event in pygame.event.get(): # User did something
+				if event.type == pygame.QUIT: # If user clicked close
+					self._done = True # Flag that we are done so we exit this loop
 
-                elif event.type == pygame.VIDEORESIZE: # window resized
-                    self._screen = pygame.display.set_mode(event.dict['size'], 
-                                               pygame.HWSURFACE|pygame.DOUBLEBUF|pygame.RESIZABLE, 32)
-                    
-            # --- Game logic should go here
+				elif event.type == pygame.VIDEORESIZE: # window resized
+					self._screen = pygame.display.set_mode(event.dict['size'], 
+											   pygame.HWSURFACE|pygame.DOUBLEBUF|pygame.RESIZABLE, 32)
+					
+			# --- Game logic should go here
+			# --- Getting frames and drawing  
+			if self._kinect.has_new_infrared_frame():
+				frame = self._kinect.get_last_infrared_frame()
+				self.draw_infrared_frame(frame, self._frame_surface)
+				frame = None
 
-            # --- Getting frames and drawing  
-            # --- Woohoo! We've got a frame! Let's fill out back buffer surface with frame's data 
-            if self._kinect.has_new_infrared_frame():
-                frame = self._kinect.get_last_color_frame()
-                self.draw_infrared_frame(frame, self._frame_surface)
-                frame = None
+			# --- Cool! We have a body frame, so can get skeletons
+			if self._kinect.has_new_body_frame(): 
+				self._bodies = self._kinect.get_last_body_frame()
 
-            # --- Cool! We have a body frame, so can get skeletons
-            if self._kinect.has_new_body_frame(): 
-                self._bodies = self._kinect.get_last_body_frame()
+			# --- draw skeletons to _frame_surface
+			if self._bodies is not None: 
+				for i in range(0, self._kinect.max_body_count):
+					body = self._bodies.bodies[i]
+					if not body.is_tracked: 
+						continue 
+					
+					joints = body.joints 
+					# convert joint coordinates to color space 
+					joint_points = self._kinect.body_joints_to_color_space(joints)
+					self.draw_body(joints, joint_points, SKELETON_COLORS[i])
 
-            # --- draw skeletons to _frame_surface
-            if self._bodies is not None: 
-                for i in range(0, self._kinect.max_body_count):
-                    body = self._bodies.bodies[i]
-                    if not body.is_tracked: 
-                        continue 
-                    
-                    joints = body.joints 
-                    # convert joint coordinates to color space 
-                    joint_points = self._kinect.body_joints_to_color_space(joints)
-                    self.draw_body(joints, joint_points, SKELETON_COLORS[i])
+			# --- copy back buffer surface pixels to the screen, resize it if needed and keep aspect ratio
+			# --- (screen size may be different from Kinect's color frame size) 
+			surface_to_draw = pygame.transform.scale(self._frame_surface, (512,424));
+			self._screen.blit(self._frame_surface, (0,0))
+			surface_to_draw = None
+			pygame.display.update()
 
-            # --- copy back buffer surface pixels to the screen, resize it if needed and keep aspect ratio
-            # --- (screen size may be different from Kinect's color frame size) 
-            h_to_w = float(self._frame_surface.get_height()) / self._frame_surface.get_width()
-            target_height = int(h_to_w * self._screen.get_width())
-            surface_to_draw = pygame.transform.scale(self._frame_surface, (self._screen.get_width(), target_height));
-            self._screen.blit(self._frame_surface, (0,0))
-            surface_to_draw = None
-            pygame.display.update()
+			# --- Go ahead and update the screen with what we've drawn.
+			pygame.display.flip()
 
-            # --- Go ahead and update the screen with what we've drawn.
-            pygame.display.flip()
+			# --- Limit to 60 frames per second
+			self._clock.tick(60)
 
-            # --- Limit to 60 frames per second
-            self._clock.tick(60)
-
-        # Close our Kinect sensor, close the window and quit.
-        self._kinect.close()
-        pygame.quit()
+		# Close our Kinect sensor, close the window and quit.
+		self._kinect.close()
+		pygame.quit()
 
 
 __main__ = "Kinect v2 InfraRed"
