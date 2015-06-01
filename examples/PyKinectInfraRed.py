a0020e10dd64f2904935c124a15b8f4653eb1344
Philippe Grassia <pgrassia@bsd.uchicago.edu>
2015-06-01 11:57:48 -0500

    infrared example

--- examples/PyKinectInfraRed.py
+++ examples/PyKinectInfraRed.py
@@ -53,62 +53,6 @@ class InfraRedRuntime(object):
 		pygame.display.set_caption("Kinect for Windows v2 Infrared")
 
 
-    def draw_body_bone(self, joints, jointPoints, color, joint0, joint1):
-        joint0State = joints[joint0].TrackingState;
-        joint1State = joints[joint1].TrackingState;
-
-        # both joints are not tracked
-        if (joint0State == PyKinectV2.TrackingState_NotTracked) or (joint1State == PyKinectV2.TrackingState_NotTracked): 
-            return
-
-        # both joints are not *really* tracked
-        if (joint0State == PyKinectV2.TrackingState_Inferred) and (joint1State == PyKinectV2.TrackingState_Inferred):
-            return
-
-        # ok, at least one is good 
-        start = (jointPoints[joint0].x, jointPoints[joint0].y)
-        end = (jointPoints[joint1].x, jointPoints[joint1].y)
-
-        try:
-            pygame.draw.line(self._frame_surface, color, start, end, 8)
-        except: # need to catch it due to possible invalid positions (with inf)
-            pass
-
-    def draw_body(self, joints, jointPoints, color):
-        # Torso
-        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_Head, PyKinectV2.JointType_Neck);
-        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_Neck, PyKinectV2.JointType_SpineShoulder);
-        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_SpineShoulder, PyKinectV2.JointType_SpineMid);
-        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_SpineMid, PyKinectV2.JointType_SpineBase);
-        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_SpineShoulder, PyKinectV2.JointType_ShoulderRight);
-        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_SpineShoulder, PyKinectV2.JointType_ShoulderLeft);
-        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_SpineBase, PyKinectV2.JointType_HipRight);
-        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_SpineBase, PyKinectV2.JointType_HipLeft);
-    
-        # Right Arm    
-        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_ShoulderRight, PyKinectV2.JointType_ElbowRight);
-        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_ElbowRight, PyKinectV2.JointType_WristRight);
-        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_WristRight, PyKinectV2.JointType_HandRight);
-        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_HandRight, PyKinectV2.JointType_HandTipRight);
-        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_WristRight, PyKinectV2.JointType_ThumbRight);
-
-        # Left Arm
-        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_ShoulderLeft, PyKinectV2.JointType_ElbowLeft);
-        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_ElbowLeft, PyKinectV2.JointType_WristLeft);
-        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_WristLeft, PyKinectV2.JointType_HandLeft);
-        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_HandLeft, PyKinectV2.JointType_HandTipLeft);
-        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_WristLeft, PyKinectV2.JointType_ThumbLeft);
-
-        # Right Leg
-        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_HipRight, PyKinectV2.JointType_KneeRight);
-        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_KneeRight, PyKinectV2.JointType_AnkleRight);
-        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_AnkleRight, PyKinectV2.JointType_FootRight);
-
-        # Left Leg
-        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_HipLeft, PyKinectV2.JointType_KneeLeft);
-        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_KneeLeft, PyKinectV2.JointType_AnkleLeft);
-        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_AnkleLeft, PyKinectV2.JointType_FootLeft);
-
 
     def draw_infrared_frame(self, frame, target_surface):
 		if frame is None:  # some usb hub do not provide the infrared image
@@ -134,34 +78,14 @@ class InfraRedRuntime(object):
 					self._screen = pygame.display.set_mode(event.dict['size'], 
 											   pygame.HWSURFACE|pygame.DOUBLEBUF|pygame.RESIZABLE, 32)
 					
-			# --- Game logic should go here
+
 			# --- Getting frames and drawing  
 			if self._kinect.has_new_infrared_frame():
 				frame = self._kinect.get_last_infrared_frame()
 				self.draw_infrared_frame(frame, self._frame_surface)
 				frame = None
 
-			# --- Cool! We have a body frame, so can get skeletons
-			if self._kinect.has_new_body_frame(): 
-				self._bodies = self._kinect.get_last_body_frame()
-
-			# --- draw skeletons to _frame_surface
-			if self._bodies is not None: 
-				for i in range(0, self._kinect.max_body_count):
-					body = self._bodies.bodies[i]
-					if not body.is_tracked: 
-						continue 
-					
-					joints = body.joints 
-					# convert joint coordinates to color space 
-					joint_points = self._kinect.body_joints_to_color_space(joints)
-					self.draw_body(joints, joint_points, SKELETON_COLORS[i])
-
-			# --- copy back buffer surface pixels to the screen, resize it if needed and keep aspect ratio
-			# --- (screen size may be different from Kinect's color frame size) 
-			surface_to_draw = pygame.transform.scale(self._frame_surface, (512,424));
 			self._screen.blit(self._frame_surface, (0,0))
-			surface_to_draw = None
 			pygame.display.update()
 
 			# --- Go ahead and update the screen with what we've drawn.
